367e0989cc539b1f54bde51c495223cc61e3e57b
sebug <sebastian.gfeller@gmail.com>
2020-02-23 16:15:01 +0100

    With corejs and non-functioning async

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -1,4 +1,9 @@
 define(["lib/knockout"], function (ko) {
+
+    async function someFun() {
+	const res = await Promise.resolve(43);
+    }
+    
     class ShellViewModel {
 	constructor() {
 	    this.message = ko.observable('Hello from Knockout component');
